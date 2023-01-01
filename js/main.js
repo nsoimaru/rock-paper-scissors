9bcbfcb768318ee2356bf9eba5793e3bdc54585c
nsoimaru <nsoimaru@gmail.com>
2023-01-01 15:33:27 +0200

    Finish logic for the game
    English is not my native language so bare width with me.
    The code is messy but I manage to understand every line of code.
    Still to fix:
    1. Logic for check the winner, some time seems to have his own logic
    2. Show who win the round
    3. Finish styling the game

--- js/main.js
+++ js/main.js
@@ -19,19 +19,117 @@ computerChoice = () => {
 }
 
 // Initiate the game
-playGame = (e) => { 
-    // Play five rounds
-    player = e.target.id
-
-    if (player === computerChoice()) {
-        console.log('tie')
-    } else {
-        console.log('it`s working!!!')
-    }
+playGame = () => {
+    buttons.forEach(button => {
+        button.addEventListener('click', playRound);
+    })    
 }
 
-buttons.forEach(button => {
-    button.addEventListener('click', playGame);
-})
+playRound = (e) => { 
+    // Play five rounds
+    player = e.target.id;
+    // player = player.toString();
+    if(round < 5) {
+        checkWinner();
+    } else {        
+        if (player === 'rock' || player === 'paper' || player === scissors) {
+            playerChoiceButton = buttons.forEach(button => {
+            button.classList.add('disabled'); 
+                            
+            let playAgainButton = document.getElementById('playAgainButton');    
+            playAgainButton.classList.remove('hidden');
+            playAgainButton.classList.remove('disabled');
 
+            let quitButton = document.getElementById('quit');
+            quitButton.classList.remove('hidden');                
+            quitButton.classList.remove('disabled');
+            })
+        } else {
+            if (player === 'playAgainButton') {
+                playAnotherRound();
+            } else {
+                quitGame();
+            }
+        }
+    }
+    // return player;
+}
 
+checkWinner = () => {
+    let winner = '';
+    let computer = computerChoice();
+
+    if (player == computer) {
+        winner = 'Tie';
+        console.log(player);
+        console.log(computer);
+        console.log(winner);
+    }
+    else if (player == 'rock' && computer == 'paper') {
+        winner = 'Computer win!';
+        computerScore++;
+        console.log(player);
+        console.log(computer);
+        console.log(winner);
+    }
+    else if (player == 'paper' && computer == 'scissors') {
+        winner = 'Computer win!';
+        computerScore++;
+        console.log(player);
+        console.log(computer);
+        console.log(winner);
+    }
+    else if (player == 'scissors' && computer == 'rock') {
+        winner = 'Computer win!';
+        computerScore++;
+        console.log(player);
+        console.log(computer);
+        console.log(winner);
+    } else {
+        winner = 'Player win!';
+        playerScore++;
+        console.log(player);
+        console.log(computer);
+        console.log(winner);
+    }
+    round++;
+
+    // const resultDiv = document.querySelector('.result');
+
+    const playerSelection = document.querySelector('.result .result-show .player-selection');
+    playerSelection.classList.add('result');
+    playerSelection.innerText = `${player}`;
+
+    const computerSelection = document.querySelector('.result .result-show .computer-selection');
+    computerSelection.classList.add('result');
+    computerSelection.innerText = `${computerChoice()}`;
+    
+    const winnerShow = document.querySelector('.result .result-show .winner-show');
+    winnerShow.classList.add('result');
+    winnerShow.innerText = `${winner}`; 
+    
+    const playerResult = document.querySelector('.result .result-show .player-score');
+    playerResult.classList.add('result');
+    playerResult.innerText = `${playerScore}`;
+
+    const computerResult = document.querySelector('.result .result-show .computer-score');
+    computerResult.classList.add('result');
+    computerResult.innerText = `${computerScore}`;
+
+    const showRound = document.querySelector('.result .result-show .round-show');
+    showRound.classList.add('result');
+    showRound.innerText = `${round}`;
+}
+
+playAnotherRound = () => {
+    console.log('Play another round');
+    window.location.reload();
+}
+
+quitGame = () => {
+    container = document.getElementsByClassName('container')
+    console.log(container);
+    container[0].innerText = 'By!!!';
+
+}
+playGame();
